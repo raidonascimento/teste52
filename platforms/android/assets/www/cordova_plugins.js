aa04c796f44b74bf5eabc913d5e2f3ab099b66f3
Fernando <fernando@sisdado.com>
2015-03-18 18:14:24 -0300

    teste52

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.triarc.sqlitePlugin/www/SQLitePlugin.js",
+        "id": "com.triarc.sqlitePlugin.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.triarc.sqlitePlugin": "1.0.3",
+    "org.apache.cordova.console": "0.2.13"
+}
+// BOTTOM OF METADATA
+});
